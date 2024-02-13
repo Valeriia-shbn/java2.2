ed893746473a3d549facc6c0d3754c1fb591cb77
Valeriia Shubina <valeriiashbn@gmail.com>
2024-02-13 03:44:16 +0300

    reformat code

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,21 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-    int accountBalance = 100;
-    int topUp = 1100;
-    int bonusTriggerValue = 1000;
-    int bonusPercent = 1;
-    if (topUp>bonusTriggerValue) {
-        int bonus = topUp * bonusPercent / 100;
-        accountBalance = accountBalance + topUp + bonus;
+        int accountBalance = 100;
+        int topUp = 1100;
+        int bonusTriggerValue = 1000;
+        int bonusPercent = 1;
+        if (topUp > bonusTriggerValue) {
+            int bonus = topUp * bonusPercent / 100;
+            accountBalance = accountBalance + topUp + bonus;
 
-        System.out.println("Итоговая сумма: " + accountBalance);
-        System.out.println("Бонусов начислено: " + bonus);
-        }
-    else {
-        accountBalance = accountBalance + topUp;
+            System.out.println("Итоговая сумма: " + accountBalance);
+            System.out.println("Бонусов начислено: " + bonus);
+        } else {
+            accountBalance = accountBalance + topUp;
 
-        System.out.println("Итоговая сумма: " + accountBalance);
-        System.out.println("Бонусов начислено: 0");
+            System.out.println("Итоговая сумма: " + accountBalance);
+            System.out.println("Бонусов начислено: 0");
         }
     }
 }
